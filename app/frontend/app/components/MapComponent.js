5547c2a8b5adcaaa2cfc0f3c52e406526d55a7ae
Carrie Streed <cmstreed@gmail.com>
2016-04-13 19:11:58 -0400

    input field is reading text

--- app/frontend/app/components/MapComponent.js
+++ app/frontend/app/components/MapComponent.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const MapComponent = React.createClass({
+
+  countryName: function(e){
+    this.props.countryInput(e);
+  },
+
   render: function(){
     return (
       <div>
         <p>This is where the Map or Input Field goes</p>
 
-        <input placeholder='enter a country' />
-        <button type='button'>Go!</button>
+        <input placeholder='enter a country' onChange={this.countryName}/>
+        <button onClick={this.props.onSubmit}>Go!</button>
       </div>
     );
   }
--- app/frontend/app/containers/SignedInPage.js
+++ app/frontend/app/containers/SignedInPage.js
@@ -3,13 +3,29 @@ import HeaderComponent from '../components/HeaderComponent';
 import MapComponent from '../components/MapComponent';
 import TopHitsComponent from '../components/TopHitsComponent';
 
+
 const SignedInPage = React.createClass({
+
+  getInitialState: function(){
+    return {
+      ajaxReturn: [],
+      name: '',
+    };
+  },
+
+  countryInput: function(e){
+    this.setState({
+      name: e.target.value
+    });
+    console.log(this.state.name);
+  },
+
   render: function() {
     return (
       <div>
         <HeaderComponent />
-        <MapComponent />
-        <TopHitsComponent />
+        <MapComponent countryInput={this.countryInput} />
+        <TopHitsComponent songs={this.state.ajaxReturn} />
       </div>
     );
   }
