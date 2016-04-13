61070f4312e727b33a5709d87ecf5a4af2c5b573
Andrew Fry <andyfry01@gmail.com>
2016-04-13 15:42:01 -0400

    fleshing out backend, have get and post routes done, haven't tested: andy

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,27 +1,91 @@
 var express = require('express');
 var cors = require('cors');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var app = express();
 
-//ability to access ajax ANYWHERE
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//connecting to mongo database
 var MongoClient = mongodb.MongoClient;
-//mongodb base url
-var mongoUrl = 'mongodb://localhost:27017/projectThree';
 
+//Mongo URL, collections
+var mongoUrl = 'mongodb://localhost:27017/projectThree';
+var usersCollection = db.collection('users');
+var songsCollection = db.collection('songs');
+
+//default get route
 app.get('/', function(request, response) {
-  response.json({"description": "hi this link is working!!!"})
+  response.json({"message": "welcome to the database!"})
 });
 
-app.get('/users')
+// route for finding a user in the database when login is clicked.
+// the object that gets sent to this path should look as follows:
+//    {
+//      'username': <username>
+//      'password': <password>
+//    }
+app.get('/users/:user', function(request, response){
+  MongoClient.connect(mongoUrl, function(error, db){
+    if (err) {
+      console.log('error connecting to db:', error);
+    } else {
+      console.log('searching database for user information');
+      // I'm going to have to experiment with this next line to actually find the user based on the username/pass fields
+      usersCollection.find(request.params).toArray(function (error, result) {
+        if (error) {
+          console.log("error", error);
+          response.json("error")
+        } else if (result.length) {
+          console.log('user found:', result);
+          response.json(result)
+        } else {
+          console.log('no users found in database with that username/password');
+          response.json('no users found in database with that username/password')
+        }
+        db.close(function(){
+          console.log("database closed");
+        }) //end db.close()
+      }) //end usersCollection.find()
+    } //end else
+  }) //end MongoClient connect
+}) // end get
+
+// route for adding a new user to the database
+app.post('/users/new', function(request,response){
+  console.log('adding new user to the users database:', request.body);
+  MongoClient.connect(mongoUrl, function(error, db){
+    if (error) {
+      console.log("error connecting to database:", error);
+    } else {
+      console.log('Adding new user to database');
+
+      //some tweaking probably required to build this object correctly
+      var newUser = {
+        'username': request.body.username,
+        'password': request.body.password,
+        'playlist': []
+      }
+
+      usersCollection.insert([newUser], function(error, result){
+        if (error) {
+          console.log('error adding new user:', error);
+        } else {
+          console.log('new user added', result);
+          response.json(result)
+        }
+        db.close(function(){
+          console.log('database closed');
+        }) //end db.close()
+      }) //end usersCollection.insert()
+    } //end else
+  }) //end MongoClient connect
+}) // end post
+
+
 
 app.listen(3000, function() {
-  console.log("hi man im listening to u at port 3k");
+  console.log("listening on port 3000");
 })
