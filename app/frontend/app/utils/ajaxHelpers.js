bc3639e113bdee7df31a2dccc524b8d11179bce2
Andrew Fry <andyfry01@gmail.com>
2016-04-16 14:37:52 -0400

    fixed playlist bug - andy

--- app/frontend/app/utils/ajaxHelpers.js
+++ app/frontend/app/utils/ajaxHelpers.js
@@ -3,11 +3,6 @@ import API_KEY from './API_KEY';
 
 const helpers = {
 
-  //get all users
-  getAllUsers: function(){
-    return axios.get('http://localhost:3000/users');
-  },
-
   //finding user by name
   findUser: function(user){
     return axios.post('http://localhost:3000/users/find', user);
@@ -35,40 +30,7 @@ const helpers = {
 
   //add song to user playlist
   addSongToPlaylist: function(song){
-    //loggedInUser = _id of logged in user
-    let songInfo = {
-      loggedInUser: undefined,
-      lastSong: undefined
-    }
-    // nasty code, delayAddSong and addSong should be refactored into a callback function
-    function delayAddSong() {
-      let timeoutID = window.setTimeout(addSong, 750);
-    }
-    function addSong() {
-      axios.post('http://localhost:3000/addSong/', songInfo);
-    }
-
-    function delayPostSong(){
-      let timeoutID = window.setTimeout(postSong(song), 500)
-    }
-    function postSong(song){
       return axios.post('http://localhost:3000/songs/new', song);
-    }
-
-    //finds logged in user
-    axios.get('http://localhost:3000/loggedin')
-    .then(function(response){
-      songInfo.loggedInUser = response
-    })
-    //find last song added to songs collection
-    axios.get('http://localhost:3000/lastsong')
-    .then(function(response){
-      songInfo.lastSong = response
-    })
-    //adds song to songs collection
-    delayPostSong();
-    //finds logged in user, adds song to playlist array
-    delayAddSong();
   }
 
 }
