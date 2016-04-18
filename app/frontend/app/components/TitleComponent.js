13ea6509b279f1d19dbf7032dd15a5ad1f9daf70
Carrie Streed <cmstreed@gmail.com>
2016-04-17 20:57:53 -0400

    trying to update logo

--- app/frontend/app/components/TitleComponent.js
+++ app/frontend/app/components/TitleComponent.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const TitleComponent = React.createClass({
   render: function(){
+    let logo = '../images/vybez.png';
 
     let logoStyle = {
       backgroundColor: 'rgba(142, 38, 113, .8)',
+      backgroundImage: 'url(' + logo + ')',
       color: 'rgba(168, 168, 168, 1)',
       borderRadius: '50%',
       width: '120px',
